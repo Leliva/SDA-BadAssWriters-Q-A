1f46bf5d401611174088814b571f43b77fbf6705
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2019-12-07 21:09:11 +0100

    Temporary controller and service.

--- src/main/java/com/sda/badAssWritersQA/badAssWritersQA/api/QuestionnaireController.java
+++ src/main/java/com/sda/badAssWritersQA/badAssWritersQA/api/QuestionnaireController.java
@@ -4,6 +4,7 @@ package com.sda.badAssWritersQA.badAssWritersQA.api;
 import com.sda.badAssWritersQA.badAssWritersQA.model.Answer;
 import com.sda.badAssWritersQA.badAssWritersQA.model.Question;
 import com.sda.badAssWritersQA.badAssWritersQA.model.Questionnaire;
+import com.sda.badAssWritersQA.badAssWritersQA.service.QuestionnaireService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -17,7 +18,12 @@ import java.util.List;
 @Controller
 public class QuestionnaireController {
 
+    private QuestionnaireService questionnaireService;
 
+    public QuestionnaireController(QuestionnaireService questionnaireService){this.questionnaireService = questionnaireService;}
 
-
+    @GetMapping
+    public ResponseEntity<List<Questionnaire>> getAllQuestionnaires(){
+        return ResponseEntity.ok(questionnaireService.getAllQuestionnaires());
+    }
 }
--- src/main/java/com/sda/badAssWritersQA/badAssWritersQA/service/QuestionnaireService.java
+++ src/main/java/com/sda/badAssWritersQA/badAssWritersQA/service/QuestionnaireService.java
@@ -1,4 +1,19 @@
 package com.sda.badAssWritersQA.badAssWritersQA.service;
 
+import com.sda.badAssWritersQA.badAssWritersQA.model.Questionnaire;
+import com.sda.badAssWritersQA.badAssWritersQA.repository.QuestionnaireRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class QuestionnaireService {
+
+    private QuestionnaireRepository questionnaireRepository;
+
+    public QuestionnaireService(QuestionnaireRepository questionnaireRepository){
+        this.questionnaireRepository = questionnaireRepository;
+    }
+
+    public List<Questionnaire> getAllQuestionnaires(){return questionnaireRepository.findAll();}
 }
