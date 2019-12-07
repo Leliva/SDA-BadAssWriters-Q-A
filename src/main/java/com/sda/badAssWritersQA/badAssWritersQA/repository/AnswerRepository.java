142c9024acb548e1d28037e5d86a01dc0aab526c
LyszczarzKrzysztof <krzysiek.lyszczarz@o2.pl>
2019-12-07 13:47:23 +0100

    Repositories, swagger-UI enabled.

---
+++ src/main/java/com/sda/badAssWritersQA/badAssWritersQA/repository/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.sda.badAssWritersQA.badAssWritersQA.repository;
+
+import com.sda.badAssWritersQA.badAssWritersQA.model.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer,Long> {
+}
---
+++ src/main/java/com/sda/badAssWritersQA/badAssWritersQA/repository/Questionnaire.java
@@ -0,0 +1,6 @@
+package com.sda.badAssWritersQA.badAssWritersQA.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Questionnaire extends JpaRepository<Questionnaire, Long> {
+}
