bb26fdbf442ab91a932fe3e5903a0718246f28aa
DykmanSzymon <szymon.dykman@gmail.com>
2019-12-15 13:21:54 +0100

    Radio buttons.

--- src/main/java/com/sda/badAssWritersQA/badAssWritersQA/model/UserResponses.java
+++ src/main/java/com/sda/badAssWritersQA/badAssWritersQA/model/UserResponses.java
@@ -3,14 +3,18 @@ package com.sda.badAssWritersQA.badAssWritersQA.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class UserResponses {
     @Id
     @GeneratedValue
     private long id;
+    @OneToOne
     private Answer answer;
+    @OneToOne
     private Question question;
+    @OneToOne
     private Questionnaire questionnaire;
 
     public UserResponses(){}
